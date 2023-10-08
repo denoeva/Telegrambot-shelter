d47c9c68d39b333c28a33bd4b8d3f9749240f18b
elena.denoeva <denoeva.elena@skyeng.ru>
2023-10-08 14:56:20 +0300

    Initial commit

---
+++ src/test/java/pro/sky/telegrambot/shelter/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.telegrambot.shelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
